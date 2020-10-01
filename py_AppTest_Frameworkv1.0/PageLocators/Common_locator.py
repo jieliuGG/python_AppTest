03473940b3b1ea8a76891314927af4be0c980b6c
jieliuGG <810030709@qq.com>
2020-10-01 22:54:49 +0800

    元素等位层

--- py_AppTest_Frameworkv1.0/PageLocators/Common_locator.py
+++ py_AppTest_Frameworkv1.0/PageLocators/Common_locator.py
@@ -4,3 +4,5 @@
 # @Author:  jsonJie
 # @Email:   810030907@qq.com
 # @File:    Common_locator.py
+from  selenium.webdriver.common.by import By
+No_setGesture = (By.XPATH,'//div[@class=""]')
--- py_AppTest_Frameworkv1.0/PageLocators/login_locator.py
+++ py_AppTest_Frameworkv1.0/PageLocators/login_locator.py
@@ -4,3 +4,10 @@
 # @Author:  jsonJie
 # @Email:   810030907@qq.com
 # @File:    login_locator.py
+class LoginLocator():
+    username_input = ()
+    password_input = ()
+    next_step = ()
+
+    get_errorMsg_from_login = ()
+    errorMsg_from_passWord = ()
